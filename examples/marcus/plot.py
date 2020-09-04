00aecf0b06d33b270c4f19c16da7eb5f12c8d973
mlap <marcuslapeyrolerie@me.com>
2020-09-04 11:56:20 +0000

    Latest progress

--- examples/marcus/plot.py
+++ examples/marcus/plot.py
@@ -6,34 +6,29 @@ import argparse
 import gym
 import gym_fishing
 import matplotlib.pyplot as plt
-from stable_baselines3 import SAC
+from stable_baselines import PPO2
+from stable_baselines.common import make_vec_env
 import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", type=float, default=0.75)
 args = parser.parse_args()
 
-env = gym.make('fishing-v1')
-model = SAC.load("sb3_sac")
+env = make_vec_env('fishing-v1', n_envs=4)
+model = PPO2.load("sb2_ppo2_recurrent")
 traj = [[0 for j in range(101)] for i in range(100)]
 reward_list = []
 for i in range(100):
-    reward_ = 0
-    obs = env.reset(init_state=args.i)
-    traj[i][0] = obs[0]
+    obs = env.reset()
+    traj[i][0] = obs.reshape(-1)
     for t in range(100):
         action, _ = model.predict(obs)
         obs, reward, done, _ = env.step(action)
-        traj[i][t+1] = obs[0]
-        reward_ += reward
-        if done:
-            break
-    reward_list.append(reward_)
-print(np.mean(reward_list))
-
+        traj[i][t+1] = obs.reshape(-1)
+#import pdb; pdb.set_trace()
 #x = np.linspace(0, 1, 100)
 #y = [model.predict(np.array([obs]))[0][0] for obs in x]
-plt.plot(np.linspace(0, 100, 101), np.mean(traj, axis=0))
+plt.plot(np.linspace(0, 100, 101), np.mean(np.mean(traj, axis=0), axis=1))
 plt.xlabel("t")
 plt.ylabel("Average N")
 plt.savefig("plot.png")
