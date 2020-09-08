7a80b2a0b8263cd60e8c579c926e3e7bf294ac9f
Carl Boettiger <cboettig@gmail.com>
2020-09-08 00:16:28 +0000

    meta

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
-from setuptools import setup, find_packages
-
+from distutils.core import setup
 setup(
-    name="gym_fishing", 
-    version="0.0.2",
-    description="Provide gym environments for reinforcement learning",
-    packages=find_packages(),
-    python_requires='>=3.6',
+  name = 'gym_fishing',
+  packages = ['gym_fishing'],
+  version = '0.0.2',
+  license='MIT',
+  description="Provide gym environments for reinforcement learning",
+  author = 'Carl Boettiger',
+  author_email = 'cboettig@gmail.com',
+  url = 'https://github.com/boettiger-lab/gym_fishing',
+  download_url = 'https://github.com/boettiger-lab/gym_fishing/archive/v0.0.2.tar.gz',
+  keywords = ['RL', 
+              'Reinforcement Learning', 
+              'Conservation', 
+              'stable-baselines',
+              'OpenAI Gym', 
+              'AI', 
+              'Artificial Intelligence'],
+  install_requires=[ 
+          'gym'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers', 
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
 )
