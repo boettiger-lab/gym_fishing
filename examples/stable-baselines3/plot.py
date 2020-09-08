a2503142a33b6a3e7b8cbe4ea8656eeb047a7c09
mlap <marcuslapeyrolerie@me.com>
2020-09-08 12:45:07 +0000

    Updating plot script

--- examples/stable-baselines3/plot.py
+++ examples/stable-baselines3/plot.py
@@ -10,27 +10,28 @@ from stable_baselines3 import SAC
 import numpy as np
 
 
-reps = 100
+reps = 5
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", type=float, default=0.75)
 args = parser.parse_args()
 
 env = gym.make('fishing-v1')
 model = SAC.load("sb3_sac")
-traj = [[0 for j in range(101)] for i in range(reps)]
-reward_list = []
+trajs = [] #List to find agg. stats on trajectories
+reward_list = [] #To find agg. stats on rewards
 for i in range(reps):
+    traj = []
     obs = env.reset()
-    traj[i][0] = obs[0]
-    for t in range(100):
+    traj.append(obs[0])
+    while True:
         action, _ = model.predict(obs)
         obs, reward, done, _ = env.step(action)
-        traj[i][t+1] = obs[0]
-#import pdb; pdb.set_trace()
-#x = np.linspace(0, 1, 100)
-#y = [model.predict(np.array([obs]))[0][0] for obs in x]
-plt.plot(np.linspace(0, 100, 101), np.mean(traj, axis=0))
+        traj.append(obs[0])
+        if done:
+            break
+    trajs.append(traj)
+    plt.plot(np.linspace(0, len(traj)-1, len(traj)), traj)
 plt.xlabel("t")
-plt.ylabel("Average N")
+plt.ylabel("N")
 plt.savefig("plot.png")
 
