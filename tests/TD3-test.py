4d1ba55849c0bde686783ce4ef862a20e66a9bc5
Carl Boettiger <cboettig@gmail.com>
2020-10-09 16:09:02 +0000

    clean test

--- tests/TD3-test.py
+++ tests/TD3-test.py
@@ -2,28 +2,26 @@ import numpy as np
 import gym
 import gym_fishing
 from stable_baselines3 import TD3
-from stable_baselines3.common.evaluation import evaluate_policy
 
-env = gym.make('fishing-v1',  file = "results/td3.csv")
+env = gym.make('fishing-v1')
 model = TD3('MlpPolicy', env, verbose=1)
 model.learn(total_timesteps=200)
 
-model.save("results/td3")
-
-model = TD3.load("results/td3")
-env = gym.make('fishing-v1',  file = "results/td3.csv")
-
-
-## Evaluate model
-mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=5)
-print("mean reward:", mean_reward, "std:", std_reward)
-
-## Simulation for visualization purposes
+## Simulate a run with the trained model, visualize result
 obs = env.reset()
 for i in range(100):
   action, _state = model.predict(obs)
   obs, reward, done, info = env.step(action)
   env.render()
+env.plot()
 
-env.plot(output = "results/td3.png")
-   # obs = env.reset()
+
+## Evaluate model
+from stable_baselines3.common.evaluation import evaluate_policy
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=5)
+print("mean reward:", mean_reward, "std:", std_reward)
+
+
+## Save and reload the model
+model.save("td3")
+model = TD3.load("td3")
