71201fe1bd4d3753509e724706dbca8e8173fb99
Carl Boettiger <cboettig@gmail.com>
2020-10-09 15:57:16 +0000

    tidy up example

--- examples/DQN.py
+++ examples/DQN.py
@@ -1,3 +1,4 @@
+## Fishing with DQN example
 import gym
 import gym_fishing
 from stable_baselines3 import DQN
@@ -9,20 +10,24 @@ env = gym.make('fishing-v0', file = "results/dqn.csv")
 # Instantiate the agent
 model = DQN('MlpPolicy', env, verbose=1)
 # Train the agent
-model.learn(total_timesteps=int(2e5))
+model.learn(total_timesteps=int(2e4))
 
-# Save the agent
-model.save("results/dqn_fish_v0")
-
-# Evaluate the agent
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
-print("mean reward:", mean_reward, "std:", std_reward)
-
-## Simulation for visualization purposes
+## Simulate / visualize a run
 obs = env.reset()
 for i in range(100):
   action, _state = model.predict(obs)
   obs, reward, done, info = env.step(action)
   env.render()
-
 env.plot("results/dqn.png")
+
+
+# Evaluate the agent
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+print("mean reward:", mean_reward, "std:", std_reward)
+
+# Record score in leaderboard
+leaderboard("PPO", mean_reward, std_reward)
+
+
+# Save the agent
+model.save("results/dqn_fish_v0")
