a4fc61e7e062f1864cceac8c59981110638c6bb1
Carl Boettiger <cboettig@gmail.com>
2020-10-29 17:22:51 +0000

    bump latest release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
   author = 'Carl Boettiger & Marcus Lapeyrolerie',
   author_email = 'cboettig@gmail.com',
   url = 'https://github.com/boettiger-lab/gym_fishing',
-  download_url = 'https://github.com/boettiger-lab/gym_fishing/archive/v0.0.3.tar.gz',
+  download_url = 'https://github.com/boettiger-lab/gym_fishing/archive/v0.0.4.tar.gz',
   keywords = ['RL', 
               'Reinforcement Learning', 
               'Conservation', 
