ff235243f407902f9e9b8e367b1125a8091b4daf
Carl <cboettig@gmail.com>
2020-11-20 17:23:48 +0000

    generalize optimal class

--- gym_fishing/models/policies.py
+++ gym_fishing/models/policies.py
@@ -1,33 +1,33 @@
 
 
 class msy:
-    def __init__(self, env):
+    def __init__(self, env, **kwargs):
       self.env = env
 
-    def predict(self, obs):
+    def predict(self, obs, **kwargs):
       msy = self.env.r * self.env.K / 4
       action = self.env.get_action(msy)
       return action, obs
 
 class escapement:
-    def __init__(self, env):
+    def __init__(self, env, **kwargs):
       self.env = env
 
-    def predict(self, obs):
-      fish_population = self.env.get_fish_population(obs)
+    def predict(self, state, **kwargs):
+      fish_population = self.env.get_fish_population(state)
       quota = max(fish_population - self.env.K / 2., 0.) 
       action = self.env.get_action(quota)
-      return action, obs
+      return action, state
 
 class user_action:
-    def __init__(self, env):
+    def __init__(self, env, **kwargs):
       self.env = env
 
-    def predict(self, obs):
-      fish_population = self.env.get_fish_population(obs)
+    def predict(self, state, **kwargs):
+      fish_population = self.env.get_fish_population(state)
       prompt = "fish population: " + str(fish_population) + ". Your harvest quota: "
       quota = input(prompt)
       action = self.env.get_action(float(quota))
-      return action, obs
+      return action, state
 
 
