c2e5e13a57bbbf1e1aaddb0754060bbae2ef47fa
Carl <cboettig@gmail.com>
2020-11-20 17:26:04 +0000

    try again

--- gym_fishing/models/policies.py
+++ gym_fishing/models/policies.py
@@ -13,21 +13,21 @@ class escapement:
     def __init__(self, env, **kwargs):
       self.env = env
 
-    def predict(self, state, **kwargs):
-      fish_population = self.env.get_fish_population(state)
+    def predict(self, obs, **kwargs):
+      fish_population = self.env.get_fish_population(obs)
       quota = max(fish_population - self.env.K / 2., 0.) 
       action = self.env.get_action(quota)
-      return action, state
+      return action, obs
 
 class user_action:
     def __init__(self, env, **kwargs):
       self.env = env
 
-    def predict(self, state, **kwargs):
-      fish_population = self.env.get_fish_population(state)
+    def predict(self, obs, **kwargs):
+      fish_population = self.env.get_fish_population(obs)
       prompt = "fish population: " + str(fish_population) + ". Your harvest quota: "
       quota = input(prompt)
       action = self.env.get_action(float(quota))
-      return action, state
+      return action, obs
 
 
