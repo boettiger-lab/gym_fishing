af2dc84fe04ff17fe4291764a59d15f126367b31
Carl <cboettig@gmail.com>
2020-12-22 17:44:03 +0000

    ugh I still don't understand python namespaces

--- gym_fishing/__init__.py
+++ gym_fishing/__init__.py
@@ -2,57 +2,57 @@ from gym.envs.registration import register
 
 register(
     id="fishing-v0",
-    entry_point="gym_fishing.envs:FishingEnv",
+    entry_point="gym_fishing.envs.fishing_env:FishingEnv",
 )
 
 
 register(
     id="fishing-v1",
-    entry_point="gym_fishing.envs:FishingCtsEnv",
+    entry_point="gym_fishing.envs.fishing_cts_env:FishingCtsEnv",
 )
 
 register(
     id="fishing-v2",
-    entry_point="gym_fishing.envs:FishingTippingEnv",
+    entry_point="gym_fishing.envs.fishing_tipping_env:FishingTippingEnv",
 )
 
 register(
     id="fishing-v4",
-    entry_point="gym_fishing.envs:FishingModelError",
+    entry_point="gym_fishing.envs.fishing_model_error:FishingModelError",
 )
 
 
 register(
     id="fishing-v5",
-    entry_point="gym_fishing.envs:Allen",
+    entry_point="gym_fishing.envs.growth_models:Allen",
 )
 
 register(
     id="fishing-v6",
-    entry_point="gym_fishing.envs:BevertonHolt",
+    entry_point="gym_fishing.envs.growth_models:BevertonHolt",
 )
 
 register(
     id="fishing-v7",
-    entry_point="gym_fishing.envs:May",
+    entry_point="gym_fishing.envs.growth_models:May",
 )
 
 register(
     id="fishing-v8",
-    entry_point="gym_fishing.envs:Myers",
+    entry_point="gym_fishing.envs.growth_models:Myers",
 )
 
 register(
     id="fishing-v9",
-    entry_point="gym_fishing.envs:Ricker",
+    entry_point="gym_fishing.envs.growth_models:Ricker",
 )
 
 register(
     id="fishing-v10",
-    entry_point="gym_fishing.envs:NonStationary",
+    entry_point="gym_fishing.envs.growth_models:NonStationary",
 )
 
 register(
     id="fishing-v11",
-    entry_point="gym_fishing.envs:ModelUncertainty",
+    entry_point="gym_fishing.envs.growth_models:ModelUncertainty",
 )
